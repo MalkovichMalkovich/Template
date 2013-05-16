1a47a627764fe1561cee4ea8ee45d96b3d2aae20
Stas Archie Granin <sgranin@gmail.com>
2013-05-16 16:51:26 +0300

    Update template.js

--- template.js
+++ template.js
@@ -1,5 +1,6 @@
 var Template =
 {
+	debug: false,
 	blocks: {},
 	vars: {},
 	pub: {},
@@ -8,26 +9,26 @@ var Template =
 	preProcessors:
 	{
 		cleaning: [/<\!--[\s\S]*?-->|\r|\t|\n|\s{2,}/gm, ''],
-		tokens: [/#\{(.+?)\}/gi, "<!--|--><!--$1--><!--|-->"],
-		ifTag: [/<if\s+(.+?)>/gi, "<!--|--><!--function(){if($1) {return [''--><!--|-->"],
-		elseIfTag: [/<\/if>\s*?<else if\s+(.+?)>/gi, "<!--|--><!--''].join('')} else if ($1) {return [''--><!--|-->"],
-		elseTag: [/<\/if>\s*?<else>/gi, "<!--|--><!--''].join('')} else {return [''--><!--|-->"],
-		ifElseCloseTag: [/<\/(if|else)>/gi, "<!--|--><!--''].join('')}}.call(this)--><!--|-->"],
+		tokens: [/#\{(.+?)\}/gi, "<!--|-->$1<!--|-->"],
+		ifTag: [/<if\s+(.+?)>/gi, "<!--|-->function(){if($1) {return [''<!--|-->"],
+		elseIfTag: [/<\/if>\s*?<else if\s+(.+?)>/gi, "<!--|-->''].join('')} else if ($1) {return [''<!--|-->"],
+		elseTag: [/<\/if>\s*?<else>/gi, "<!--|-->''].join('')} else {return [''<!--|-->"],
+		ifElseCloseTag: [/<\/(if|else)>/gi, "<!--|-->''].join('')}}.call(this)<!--|-->"],
 		foreachTag: [/<foreach\s+(.+?)(?:\s+in\s+(.+?)(?:\s+as\s+(.+?))?)?>/gi, function(match, p1, p2, p3)
 		{
 			p3 = p3 || 't._item'; // iterrated item variable name
 			p2 = p2 || p1; // iterated structure variable name
 			p1 = p1 || 't._index'; // iterrator index name
 			
-			return "<!--|--><!--function(struct){var t=struct, _t="+p2+", _r=[]; if (_t && _t.length){for(var i=0, l=_t.length; i<l; i++){t=_t[i]; t._parent=struct; "+p1+" = i; "+p3+" = t; _r.push([''--><!--|-->";
+			return "<!--|-->function(struct){var t=struct, _t="+p2+", _r=[]; if (_t && _t.length){for(var i=0, l=_t.length; i<l; i++){t=_t[i]; t._parent=struct; "+p1+" = i; "+p3+" = t; _r.push([''<!--|-->";
 		}],
-		foreachCloseTag: [/<\/foreach>/gi, "<!--|--><!--''].join(''));} return _r.join('')}}.call(this, t)--><!--|-->"],
-		forTag: [/<for\s+(.+?)>/gi, "<!--|--><!--function(){var _r=[]; for($1){_r.push([''--><!--|-->"],
-		forCloseTag: [/<\/for>/gi, "<!--|--><!--''].join(''));} return _r.join('')}.call(this)--><!--|-->"],
-		scriptTag: [/<script(?:\s.+?)?>(.*?)<\/script>/gim, "<!--|--><!--function(){$1}.call(this)--><!--|-->"],
+		foreachCloseTag: [/<\/foreach>/gi, "<!--|-->''].join(''));} return _r.join('')}}.call(this, t)<!--|-->"],
+		forTag: [/<for\s+(.+?)>/gi, "<!--|-->function(){var _r=[]; for($1){_r.push([''<!--|-->"],
+		forCloseTag: [/<\/for>/gi, "<!--|-->''].join(''));} return _r.join('')}.call(this)<!--|-->"],
+		scriptTag: [/<script(?:\s.+?)?>(.*?)<\/script>/gim, "<!--|-->function(){$1}.call(this)<!--|-->"],
 		linkedBlockTag: [/<@(.+?)(?:\s+(.+?))?\s?\/>/gi, function(match, p1, p2)
 		{
-			return (p2) ? "<!--|--><!--this._rd('"+p1+"', "+p2+")--><!--|-->" : "<!--|--><!--this._rd('"+p1+"', t."+p1+"||t)--><!--|-->";
+			return (p2) ? "<!--|-->this._rd('"+p1+"', "+p2+")<!--|-->" : "<!--|-->this._rd('"+p1+"', t."+p1+"||t)<!--|-->";
 		}]	
 	},
 
@@ -51,7 +52,7 @@ var Template =
 	{
 		var parsedBlocks = {};
 		var foundBlock = null;
-		var preProcessor, rawBlock, parsedBlock, tokenName;
+		var preProcessor, rawBlock, parsedBlock, particle;
 
 		for (var name in this.preProcessors)
 		{
@@ -75,22 +76,19 @@ var Template =
 
 				for (var i=0, l=rawBlock.length; i<l; i++)
 				{ 
-					if (i%2 === 1)
+					particle = rawBlock[i];
+					
+					if (particle)
 					{
-						tokenName = rawBlock[i].substring(4, rawBlock[i].length-3);
-
-						tokenName = tokenName.replace(/\$([A-Z0-9_\.]+)/gi, "t.$1");
-						tokenName = tokenName.replace(/\@([A-Z0-9_\.]+)/gi, "this._fv(t, '$1')");
-						tokenName = tokenName.replace(/\^([A-Z0-9_\.]+)/gi, "this.vars.$1");
-
-						parsedBlock.push(tokenName);    
-					}
-					else
-					{   
-						if (rawBlock[i])
+						if (i%2 === 1) //token
 						{
-							parsedBlock.push("'"+rawBlock[i].replace(/'/g, "\\'")+"'");
-						}        
+							parsedBlock.push(particle.replace(/\$([A-Z0-9_\.]+)/gi, "t.$1").replace(/\@([A-Z0-9_\.]+)/gi, "this._fv(t, '$1')").replace(/\^([A-Z0-9_\.]+)/gi, "this.vars.$1"));    
+						}
+						else
+						{   
+							
+							parsedBlock.push("'"+particle.replace(/'/g, "\\'")+"'");     
+						}
 					}
 				}
 
